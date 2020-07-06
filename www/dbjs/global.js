1e5573f4eed14baeb3bd341d855e50597f7e2de4
Shivam Medisoft Services Pvt Ltd <67900006+shivam-medisoft@users.noreply.github.com>
2020-07-06 17:39:37 +0530

    Add files via upload

---
+++ www/dbjs/global.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var portnum=" ";
+if(localStorage.ipadrs== undefined ||localStorage.ipadrs == "")
+{
+  
+    portnum=""; 
+}
+
+else
+{
+    portnum=localStorage.ipadrs;
+}
+ 
---
+++ www/dbjs/ipaddress.js
@@ -0,0 +1,9 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function geturl(){
+    var url = $('#txtUrl').val();
+    localStorage.ipadrs = url;
+}
